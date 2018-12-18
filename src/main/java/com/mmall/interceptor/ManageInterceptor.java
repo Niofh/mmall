47b38ba2211479ea818148c3c90b50dd125a81e9
Nicofh <598220654@qq.com>
2018-12-18 22:16:37 +0800

    把富文本的接口拦截改下

--- src/main/java/com/mmall/interceptor/ManageInterceptor.java
+++ src/main/java/com/mmall/interceptor/ManageInterceptor.java
@@ -3,17 +3,20 @@ package com.mmall.interceptor;
 import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
+import com.mmall.common.jedis.RedisShardedPoolUtil;
 import com.mmall.pojo.User;
 import com.mmall.utli.CookieUtil;
 import com.mmall.utli.InterceptorRespoenUtli;
 import com.mmall.utli.JsonUtil;
-import com.mmall.common.jedis.RedisShardedPoolUtil;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 管理系统全局拦截器
@@ -21,8 +24,6 @@ import javax.servlet.http.HttpServletResponse;
 public class ManageInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
-
-
 
 
         // 判断cookies 是否存在token
@@ -37,14 +38,39 @@ public class ManageInterceptor implements HandlerInterceptor {
             return false;
         }
 
+
+        // 排除拦截可以在这里写，不用写在springWeb.xml排除拦截
+        HandlerMethod handlerMethod = (HandlerMethod) o;
+        String methodName = handlerMethod.getMethod().getName();
+        String className = handlerMethod.getBean().getClass().getSimpleName(); // 包含包名
+
+        if (StringUtils.equals("UserController", className) && StringUtils.equals("login", methodName)) {
+            // 登录放开拦截
+            return true;
+        }
+
         String data = RedisShardedPoolUtil.get(loginToken);
 
         User user = JsonUtil.string2Obj(data, User.class);
         // 用户登录信息过期
         if (user == null) {
-            ServerResponse<String> serverResponse = ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户登录信息已过期");
 
-            InterceptorRespoenUtli.sendJson(httpServletResponse, serverResponse);
+            if (StringUtils.equals("ProductManageController", className) && StringUtils.equals("richtextImg_Upload", methodName)) {
+                // 富文本处理
+                Map map = new HashMap<String, String>();
+
+                map.put("success", false);
+                map.put("msg", "请登录，管理员");
+
+                InterceptorRespoenUtli.sendJson(httpServletResponse, map);
+
+            } else {
+
+                ServerResponse<String> serverResponse = ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户登录信息已过期");
+
+                InterceptorRespoenUtli.sendJson(httpServletResponse, serverResponse);
+            }
+
 
             return false;
         }
@@ -54,8 +80,20 @@ public class ManageInterceptor implements HandlerInterceptor {
         // 角色不是管理员也拒绝访问
         if (role != Const.Roles.ROLE_ADMIN) {
 
-            ServerResponse<String> serverResponse = ServerResponse.createByErrorMessage("该用户不是管理员,无法有访问权限");
-            InterceptorRespoenUtli.sendJson(httpServletResponse, serverResponse);
+            if (StringUtils.equals("ProductManageController", className) && StringUtils.equals("richtextImg_Upload", methodName)) {
+                // 富文本处理
+                Map map = new HashMap<String, String>();
+
+                map.put("success", false);
+                map.put("msg", "你不是管理员，无权限操作");
+
+                InterceptorRespoenUtli.sendJson(httpServletResponse, map);
+
+            } else {
+                ServerResponse<String> serverResponse = ServerResponse.createByErrorMessage("该用户不是管理员,无法有访问权限");
+                InterceptorRespoenUtli.sendJson(httpServletResponse, serverResponse);
+            }
+
 
             return false;
         }
